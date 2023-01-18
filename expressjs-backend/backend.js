c8723f85161bd0a82654ca9f54410f9037ab4834
Daryl Ng <daryl.ng154@gmail.com>
2023-01-18 13:49:14 -0800

    added filter by name & job

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,3 +1,5 @@
+// TODO: implement request to GET all users matching given name and job
+
 const express = require('express');
 const app = express();
 const port = 5000;
@@ -10,10 +12,22 @@ app.listen(port, () => {
 
 const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => { 
+    return users['users_list'].filter( (user) => user['job'] === job); 
+} 
+
+const filterUsers = (req_query, field, list) => { 
+    return list.filter( (user) => user[field] === req_query); 
 } 
 
 function findIndexById(id) {
-    return users['users_list'].indexOf( (user) => user['id'] === id); // or line below
+    return users['users_list'].indexOf( (user) => user['id'] === id);
+}
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
 }
 
@@ -21,10 +35,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// returns all users matching id or name or job using querys
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+    if (name != undefined && job != undefined)
+    {
+        let result = filterUsers(job, 'job', findUserByName(name)); // or below
+        // let result = findUserByName(name).filter( (user) => user['job'] === job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (name != undefined)
+    {
         let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (job != undefined)
+    {
+        let result = findUserByJob(job);
         result = {users_list: result};
         res.send(result);
     }
@@ -44,11 +74,6 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
-function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
-}
-
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
@@ -63,10 +88,10 @@ app.delete('/users/:id', (req, res) => {
     const id = req.params['id']; //or req.params.id
     let result = findIndexById(id);
     if (result === undefined)
-        res.status(204).send('Resource not found.');    //204 (No content)
+        res.status(404).send('Resource not found.');    //204 (No content)
     else {
         deleteUser(result);
-        res.status(200).end();
+        res.status(204).end();
     }
 });
 
