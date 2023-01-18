c96287f5318ec7c90cda79034ae69246ea69d647
Daryl Ng <daryl.ng154@gmail.com>
2023-01-17 17:44:20 -0800

    added APIs: GET(users), GET(users/:id), POST(users), DELETE(users/:id)

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -4,17 +4,75 @@ const port = 5000;
 
 app.use(express.json());
 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}); 
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+} 
+
+function findIndexById(id) {
+    return users['users_list'].indexOf( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});  
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findIndexById(id);
+    if (result === undefined)
+        res.status(204).send('Resource not found.');    //204 (No content)
+    else {
+        deleteUser(result);
+        res.status(200).end();
+    }
+});
+
+function deleteUser(user){
+    users['users_list'].splice(user);
+}
 
 const users = { 
     users_list :
